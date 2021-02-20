f2e3100f83dd75a9fd0fa63b3f224dfba2cbe9d8
ThomasHEBRARD <thomas.hebrard134@gmail.com>
2021-02-20 12:39:40 +0100

    quick fixes

--- business/bottle/bottle/views.py
+++ business/bottle/bottle/views.py
@@ -2,7 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from .serializers import BottleSerializer
 from business.bottle.bottle.models import Bottle
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -12,9 +11,7 @@ class BottleViewSet(ModelViewSet):
     queryset = Bottle.objects.all()
     serializer_class = BottleSerializer
     pagination_class = LimitOffsetPagination
-
     permission_classes = (IsAuthenticated,)
-    authentication_class = JSONWebTokenAuthentication
 
     def get_queryset(self):
         return self.queryset.order_by("id")
--- wine_backend/settings.py
+++ wine_backend/settings.py
@@ -114,8 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
