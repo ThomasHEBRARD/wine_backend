e469d1255161d06dd72cdf0a45d380f5c9902558
ThomasHEBRARD <thomas.hebrard134@gmail.com>
2021-02-21 15:33:34 +0100

    search input

--- business/bottle/bottle/views.py
+++ business/bottle/bottle/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from .serializers import BottleSerializer
 from business.bottle.bottle.models import Bottle
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -12,6 +13,8 @@ class BottleViewSet(ModelViewSet):
     serializer_class = BottleSerializer
     pagination_class = LimitOffsetPagination
     permission_classes = (IsAuthenticated,)
+    filter_backends = [SearchFilter]
+    search_fields = ["bottle_collection__name", "bottle_collection__millesime"]
 
     def get_queryset(self):
         return self.queryset.order_by("id")
